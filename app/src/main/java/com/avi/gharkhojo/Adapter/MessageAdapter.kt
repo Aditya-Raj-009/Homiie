81ba7cbae82cf3696caf68026d9a6404c2b5b851
Avijeet#21 <80438115+Avijeet341@user.noreply.github.com>
2024-09-01 14:00:20 +0530

    responsive layout and FilterFragment

--- app/src/main/java/com/avi/gharkhojo/Adapter/MessageAdapter.kt
+++ app/src/main/java/com/avi/gharkhojo/Adapter/MessageAdapter.kt
@@ -3,6 +3,7 @@ package com.avi.gharkhojo.Adapter
 import android.content.Context
 import android.view.LayoutInflater
 import android.view.View
+
 import android.view.ViewGroup
 import androidx.recyclerview.widget.RecyclerView
 import com.avi.gharkhojo.Model.Message
